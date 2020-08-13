7c45e446b90fa8179c38572e2f7a7210452e8ca6
churaaj <37325562+churaaj@users.noreply.github.com>
2020-08-13 09:07:56 -0700

    Create Breaking_string.c

---
+++ Breaking_string.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main() {
+    char str[] = "my_name_is_edcast_future_skills";
+    int i = 0;
+    int counter = 0;
+   
+    for(i=0;str[i]!='\0';i++){
+        int next = i + 1;
+        if(str[next] =='\0')
+        {
+            printf("%c",str[i]);
+            printf("\"");
+            printf(".");
+            break;
+        }
+        
+        if (counter == 0)
+        {
+            printf("\"");
+            counter++;
+        }
+        
+        if(counter >0 && str[i]!= '_'){
+            printf("%c",str[i]);
+        }
+        
+        if(str[i] == '_'){
+            printf("\"");
+            printf(",");
+            counter = 0;
+        }
+    }
+}
